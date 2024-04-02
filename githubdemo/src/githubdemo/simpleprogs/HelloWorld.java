b256631c26b7aa0d7543f395f19113d3680b2b57
nivasPatilgitcloud <patil.nivas2238@gmail.com>
2024-04-02 17:07:53 +0530

    added print ststement

--- githubdemo/src/githubdemo/simpleprogs/HelloWorld.java
+++ githubdemo/src/githubdemo/simpleprogs/HelloWorld.java
@@ -5,6 +5,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Helo World");
+		System.out.println("modified data");
 	}
 
 }
